6b62e351d7303fbf6fa55db93960a9361a7376f7
Carl Zimmerman <carlpzimmerman@gmail.com>
2016-11-19 20:45:16 -0500

    first commit

---
+++ spec/models/auction_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Auction, :type => :model do
+  it "is valid with valid attributes" do
+    expect(Auction.new(title: 'Anything')).to be_valid
+  end
+  it "is not valid without a title" do
+    auction = Auction.new(title: nil)
+    expect(auction).to_not be_valid
+  end
+  it "is not valid without a description"
+  it "is not valid without a start_date"
+  it "is not valid without a end_date"
+end
